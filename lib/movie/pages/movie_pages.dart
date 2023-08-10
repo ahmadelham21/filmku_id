f5a472221ce6183825209c01ed6f2bc5287ab898
ahmadelham21 <ahmadelham21@gmail.com>
2023-08-10 13:59:27 +0800

    perubahan pages

--- lib/movie/pages/movie_pages.dart
+++ lib/movie/pages/movie_pages.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:filmku_id/movie/providers/movie_get_discover_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../../app_constant.dart';
 
 class MoviePage extends StatelessWidget {
   const MoviePage({super.key});
@@ -9,35 +11,50 @@ class MoviePage extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<MovieGetDiscoverProvider>().getDiscover(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Movie DB'),
-      ),
-      body: Consumer<MovieGetDiscoverProvider>(
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(),
+        WidgetDiscoverMovie(),
+      ],
+    ));
+  }
+}
+
+class WidgetDiscoverMovie extends SliverToBoxAdapter {
+  @override
+  // TODO: implement child
+  Widget? get child => Consumer<MovieGetDiscoverProvider>(
         builder: (_, provider, __) {
           if (provider.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return Container(
+              child: Text(
+                'Loading',
+              ),
             );
           }
 
           if (provider.movies.isNotEmpty) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final movie = provider.movies[index];
-
-                return ListTile(
-                  title: Text(movie.title),
-                  subtitle: Text(movie.overview),
-                );
-              },
-            );
+            return CarouselSlider.builder(
+                itemCount: provider.movies.length,
+                itemBuilder: (_, index, __) {
+                  final movie = provider.movies[index];
+                  // print('${AppConstants.imageUrlW500}${movie.backdropPath}');
+                  return Image.network(
+                      '${AppConstants.imageUrlW500}${movie.backdropPath}');
+                },
+                options: CarouselOptions(
+                  viewportFraction: 0.8,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ));
           }
 
-          return const Center(
-            child: Text('not found discover'),
+          return Container(
+            child: const Text('not found discover'),
           );
         },
-      ),
-    );
-  }
+      );
 }
